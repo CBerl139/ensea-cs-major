70dfa42218517d29ca950c0469a46bcdbed5a447
ensea <ensea@StudentLab>
2023-12-04 09:56:34 +0100

    finished question 4

--- src/question4/question4.c
+++ src/question4/question4.c
@@ -4,17 +4,20 @@
 #define DEFAULT_MESSAGE "\nnot a function"
 #define MAX_INPUT_SIZE 100
 
-#include "question3.h"
+#include "question4.h"
 
-int main(int argc, char *argv){
+int main(int argc, char **argv){
+	int status;
 	char userInput[MAX_INPUT_SIZE];
+	char signalexitCode[100];
+
 
 	//display welcome message
 	write(STDOUT_FILENO,WELCOME_MESSAGE,strlen(WELCOME_MESSAGE));
+	//display "enseash % "
+	write(STDOUT_FILENO,WAITING_FOR_INPUT_MESSAGE,strlen(WAITING_FOR_INPUT_MESSAGE));
 	
 	while (1){
-		//display "enseash % "
-		write(STDOUT_FILENO,WAITING_FOR_INPUT_MESSAGE,strlen(WAITING_FOR_INPUT_MESSAGE));
 		//read user input
 		read(STDOUT_FILENO,userInput,MAX_INPUT_SIZE);
 		userInput[strcspn(userInput, "\n")] = '\0';
@@ -27,11 +30,22 @@ int main(int argc, char *argv){
 		//fork to keep the program running after a function call
 		pid_t pid = fork();
 		
-		if (pid == 0){
+		if (pid == 0){			
 			//execute function inputed by user
-			execlp(userInput,userInput,NULL);
-			return EXIT_SUCCESS;
+			execlp(userInput,userInput,NULL);			
+			return EXIT_FAILURE;
 		}
-		wait(NULL);
+		wait(&status);
+		
+		//display exit or signal code
+		if (WIFEXITED(status)){
+			snprintf(signalexitCode,MAX_INPUT_SIZE,"enseash [exit:%d] ",WEXITSTATUS(status));
+			write(STDOUT_FILENO,signalexitCode,strlen(signalexitCode));
+		} else if(WIFSIGNALED(status)){
+			snprintf(signalexitCode,MAX_INPUT_SIZE,"enseash [signal:%d] ",WTERMSIG(status));
+			write(STDOUT_FILENO,signalexitCode,strlen(signalexitCode));
+		}
+		
+		write(STDOUT_FILENO,"% ",strlen("% "));
 	}
 }
